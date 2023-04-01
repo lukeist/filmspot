e6f93eda55db9cc864883742e6cf6501c6ffedc7
hiluan <apt.muenchen@gmail.com>
2023-03-31 17:11:09 -0700

    New Trailer

--- src/components/Trailers.jsx
+++ src/components/Trailers.jsx
@@ -28,7 +28,7 @@ const Trailers = () => {
       {isDesktop ? (
         <iframe
           title="Trailers"
-          src="https://www.youtube.com/embed/7NUZ_SDiG2o&t=6?autoplay=1&mute=1&controls=0&loop=1&playlist=7NUZ_SDiG2o"
+          src="https://www.youtube.com/embed/TZ_EndudJ8k&t=6?autoplay=1&mute=1&controls=0&loop=1&playlist=TZ_EndudJ8k"
           allowFullScreen
           allow="autoplay; encrypted-media"
           style={{
